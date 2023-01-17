f77c62c7732ad9115888bf7913163fa5aec1cf11
artve1986 <artve1986@gmail.com>
2023-01-17 15:49:33 +0200

    Додано приклад API get запиту

--- one.py
+++ one.py
@@ -1,1 +1,9 @@
-prin('Нічого')
+import requests
+'''Приклад api get запросу. Положення космічної станції'''
+
+api_url = 'http://api.open-notify.org/iss-now.json'
+res = requests.get(api_url)
+if res.status_code == 200:   #200 показує, що запрос вдалий
+    print(res.text)
+else: print(res.status_code)
+
