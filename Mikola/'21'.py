0a1fc96954ceab6c95bb0ff5b9c3b594146ee654
artve1986 <artve1986@gmail.com>
2023-02-08 17:24:44 +0200

    вчусь використовувати dotnv

--- Mikola/'21'.py
+++ Mikola/'21'.py
@@ -1,22 +1,5 @@
-import random
-
-from aiogram import Bot, Dispatcher, executor
-from aiogram.types import Message
-from aiogram.dispatcher.filters import Text
-
-with open('Mikolaj.txt', 'r', encoding='utf - 8') as F:
-    tok = F.read()
-
-Bot_TOKEN = tok #токен бота
-
-bot: Bot = Bot(Bot_TOKEN)
-dp: Dispatcher = Dispatcher(bot)
-
-deck = {
-        '6♥': 6, '7♥': 7,'8♥': 8, '9♥': 9, '10♥': 10, 'J♥': 2, 'Q♥': 3, 'K♥': 4, 'A♥': 11,
-        '6♦': 6, '7♦': 7,'8♦': 8, '9♦': 9, '10♦': 10, 'J♦': 2, 'Q♦': 3, 'K♦': 4, 'A♦': 11,
-        '6♠': 6, '7♠': 7,'8♠': 8, '9♠': 9, '10♠': 10, 'J♠': 2, 'Q♠': 3, 'K♠': 4, 'A♠': 11,
-        '6♣': 6, '7♣': 7,'8♣': 8, '9♣': 9, '10♣': 10, 'J♣': 2, 'Q♣': 3, 'K♣': 4, 'A♣': 11
-        }
-
-print(type(deck))
+import os
+import dotenv
+dotenv.load_dotenv()
+tok = os.getenv('Bot_tok')
+print(tok)
